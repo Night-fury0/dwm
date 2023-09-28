08c8798e7689b37cff633d69a0ae16c2e2bd0d9f
sriram <sriram19990127@gmail.com>
2023-09-29 00:22:24 +0530

    changed font

--- config.h
+++ config.h
@@ -14,8 +14,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char *fonts[]          = { "Liberation Mono:size=14" };
+static const char dmenufont[]       = "Liberation Mono:size=14";
 static const char col_white[]       = "#FFFFFF";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
